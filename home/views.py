92baa40275a98e03d3c635762d18645442279f06
CristianHMartinez <cristian.martinez.716cm9@gmail.com>
2025-07-19 10:24:10 -0600

    re migrate

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from catalog.models import Book
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', {
+        'books': Book.objects.all()  # Fetch all books from the database
+    })
